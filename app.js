54f4bc0e4b85ad1cb6f32c0927dde862239278d3
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2023-04-08 03:00:38 +0530

    added urls in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const microphoneButton = document.getElementById("microphoneButton");
 
 microphoneButton.addEventListener("click", function () {
 	window.open(
-		"http://127.0.0.1:5501/mic/",
+		"https://peppi2-84n95g2s3-creator79.vercel.app/mic/",
 		"nativeWindow",
 		"width=350,height=300"
 	);
